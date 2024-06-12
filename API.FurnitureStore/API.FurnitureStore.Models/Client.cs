770218322cf810cc6ca55d603607d92fdc17597d
Joaquin Tealdi <joaquintealdi25@gmail.com>
2024-06-12 19:33:20 -0300

    add models layer

---
+++ API.FurnitureStore/API.FurnitureStore.Models/Client.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.FurnitureStore.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public int Address { get; set; }
+    }
+}
