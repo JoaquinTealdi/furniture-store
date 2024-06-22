9feccb76e065ea5ab37532b4256bb0a796731712
Joaquin Tealdi <joaquintealdi25@gmail.com>
2024-06-22 20:32:25 -0300

    add DbContext Service

--- API.FurnitureStore/API.FurnitureStore.API/Program.cs
+++ API.FurnitureStore/API.FurnitureStore.API/Program.cs
@@ -1,3 +1,6 @@
+using API.FurnitureStore.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<FugnitureStoreDbContext>(options =>
+        options.UseSqlite(builder.Configuration.GetConnectionString("FurnitureConnection")));
 
 var app = builder.Build();
 
---
+++ API.FurnitureStore/API.FurnitureStore.Data/FugnitureStoreDbContext.cs
@@ -0,0 +1,21 @@
+﻿using API.FurnitureStore.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.FurnitureStore.Data
+{
+    public class FugnitureStoreDbContext : DbContext
+    {
+        public FugnitureStoreDbContext(DbContextOptions options) : base(options) { }
+
+
+        DbSet<Client> Clients { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Product> Products { get; set; }
+        DbSet<ProductCategory> ProductCategories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite();
+        }
+    }
+}
