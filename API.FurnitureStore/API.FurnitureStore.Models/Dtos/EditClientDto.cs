a758313e5fe7f6783242c174355115c933393533
Joaquin Tealdi <joaquintealdi25@gmail.com>
2025-01-07 18:26:49 -0300

    add create and edit client dto add response model

---
+++ API.FurnitureStore/API.FurnitureStore.Models/Dtos/EditClientDto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace API.FurnitureStore.Models.Dtos
+{
+    public class EditClientDto
+    {
+        [Required]
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Address { get; set; }
+    }
+}
