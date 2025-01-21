b04433768d7b7e812f7e3c888573332f1e6bafe2
Joaquin Tealdi <joaquintealdi25@gmail.com>
2025-01-21 17:35:44 -0300

    add IOrdersService

---
+++ API.FurnitureStore/API.FurnitureStore.Services/Interfaces/IOrdersService.cs
@@ -0,0 +1,20 @@
+﻿using API.FurnitureStore.Models.Dtos.Product;
+using API.FurnitureStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using API.FurnitureStore.Models.Dtos.Order;
+
+namespace API.FurnitureStore.Services.Interfaces
+{
+    public interface IOrdersService
+    {
+        Task<IEnumerable<Order>> GetOrders();
+        Task<Order?> GetOrderById(int id);
+        Task<OperationResult> CreateOrder(CreateOrderDto order);
+        Task<OperationResult> EditOrder(EditOrderDto order);
+        Task<OperationResult> DeleteOrder(int id);
+    }
+}
