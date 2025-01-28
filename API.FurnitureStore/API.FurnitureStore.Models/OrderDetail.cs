9c8dcd9f51e1ba9c797a4c46d749fdff20d7d2f2
Joaquin Tealdi <joaquintealdi25@gmail.com>
2025-01-28 10:00:48 -0300

    update model

--- API.FurnitureStore/API.FurnitureStore.Models/OrderDetail.cs
+++ API.FurnitureStore/API.FurnitureStore.Models/OrderDetail.cs
@@ -11,5 +11,7 @@ namespace API.FurnitureStore.Models
         public int Id { get; set; }
         public int Quantity { get; set; }
         public Product Product { get; set; }
+        public int OrderId { get; set; }
+        public Order Order { get; set; }
     }
 }
